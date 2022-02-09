d1ea60b8564957a4d01c5626c5120bf27d3436d5
heejin <huhheejin249@gmail.com>
2022-02-09 23:44:09 +0900

    jQuery 등록 부분 안 됨

--- src/main/resources/static/js/checkList-validation.js
+++ src/main/resources/static/js/checkList-validation.js
@@ -6,10 +6,10 @@ function invalid(){
     let check = true;
     let toDoValid = true;
 
-    const regToDo = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    //한글 영문 숫자 띄어쓰기 가능
+    const regToDo = /^[가-힣a-zA-Z0-9\s]+$/;
 
-
-    //이메일 형식 확인
+    //'할 일' 형식 확인
     if (regToDo.test($("#todo").val()) === false) {
         console.log(1);
         $("#todo").addClass('is-invalid');
@@ -18,7 +18,7 @@ function invalid(){
 
         toDoValid = false;
     }
-    //이메일 값 null 확인
+    //'할 일' 값 null 확인
     if ($("#todo").val() == '') {
         console.log(2);
         $("#todo").addClass('is-invalid');
@@ -26,16 +26,14 @@ function invalid(){
         $(".valid-feedback").text("입력 바랍니다.");
         toDoValid = false;
     }
-
-    //이메일 유효성 통과시 유효성 success 처리
+    //할 일 유효성 통과시 유효성 success 처리
     if (toDoValid) {
         console.log(5);
         $("#todo").removeClass('is-invalid');
         $("#todo").addClass('is-valid');
     }
 
-
-    //이메일, 비밀번호 유효성 둘 중 하나라도 실패 시 false 리턴
+    //할 일 유효성 실패 시 false 리턴
     if (!toDoValid) check = false;
     return check;
 }
